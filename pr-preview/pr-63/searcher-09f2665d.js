3b29850dd1d307f20eae706ff13855294d2d941d
joraff <joraff@users.noreply.github.com>
2026-06-12 16:42:29 +0000

    Deploy preview for PR 63 🛫

--- pr-preview/pr-63/searcher-09f2665d.js
+++ pr-preview/pr-63/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-35a46407.js',
+                path_to_root + 'searchindex-8d5581ce.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
