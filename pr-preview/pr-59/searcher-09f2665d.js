b6426305527e8e780617a3239ffc5a3b758ff476
bronius <bronius@users.noreply.github.com>
2026-06-15 19:24:16 +0000

    Deploy preview for PR 59 🛫

--- pr-preview/pr-59/searcher-09f2665d.js
+++ pr-preview/pr-59/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-0dfc1a0f.js',
+                path_to_root + 'searchindex-9ed860a7.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
