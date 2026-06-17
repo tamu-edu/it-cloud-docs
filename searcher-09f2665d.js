160539bd6afc9db2f5fdffe33bed8963d34b82d5
joraff <joraff@gmail.com>
2026-06-17 18:45:46 +0000

    Deploying to gh-pages from @ tamu-edu/it-cloud-docs@bcbc8e237b7f3013e1973c869f3c0a8dcb49df2a 🚀

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-05cfd3d6.js',
+                path_to_root + 'searchindex-e96f2f67.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
